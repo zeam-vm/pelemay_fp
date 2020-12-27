9e78d703f88ec623acfe3561c9463b7153ee8284
Susumu Yamazaki <zacky1972@gmail.com>
2020-12-27 10:53:28 +0900

    add version, package and description

--- mix.exs
+++ mix.exs
@@ -1,11 +1,15 @@
 defmodule PelemayFp.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :pelemay_fp,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.11",
+      package: package(),
+      description: "Fast parallel map function for Elixir",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -24,5 +28,13 @@ defmodule PelemayFp.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
+  end
+
+  defp package do
+    %{
+      licenses: ["Apache 2"],
+      maintainers: ["Susumu Yamazaki"],
+      links: %{"GitHub" => "https://github.com/zeam-vm/pelemay_fp"}
+    }
   end
 end
