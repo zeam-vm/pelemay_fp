eac133b3dd14c3e64ee140262120c146f01bf0ef
Susumu Yamazaki <zacky1972@gmail.com>
2020-12-27 14:54:51 +0900

    add PelemayFp.map and PelemayFp.map_chunk

--- test/pelemay_fp_test.exs
+++ test/pelemay_fp_test.exs
@@ -1,4 +1,13 @@
 defmodule PelemayFpTest do
   use ExUnit.Case
   doctest PelemayFp
+
+  test "PlemeayFp.map" do
+    assert PelemayFp.map(1..100, &(&1 * 2), 10) == Enum.map(1..100, &(&1 * 2))
+  end
+
+  test "PlemeayFp.ma_chunk" do
+    assert PelemayFp.map_chunk(1..100, &(&1 * 2), fn x -> Enum.map(x, &(&1 * 2)) end, 10) ==
+             Enum.map(1..100, &(&1 * 2))
+  end
 end
