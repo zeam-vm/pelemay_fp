f40e508fc820a5cf54b17e28de099dc6bc330e34
Susumu Yamazaki <zacky1972@gmail.com>
2020-12-27 10:41:51 +0900

    add description of PelemayFp

--- lib/pelemay_fp.ex
+++ lib/pelemay_fp.ex
@@ -1,5 +1,12 @@
 defmodule PelemayFp do
   @moduledoc """
-  Documentation for `PelemayFp`.
+  **Pelemay Fast Pmap**: provides fast `pmap` (Parallel `map` function), similar to the `Enum` module, although computations will be executed in parallel using `Process.spawn/4`.
+
+  Here is a quick example on how to calculate the square of each element with PelemayFp:
+
+  ```elixir
+  list
+  |> PelemayFp.pmap(& &1 * &1)
+  ```
   """
 end
