97e99881e9d32a369a0f988d6d8821c9c0171068
Susumu Yamazaki <zacky1972@gmail.com>
2020-12-27 12:30:09 +0900

    apply mix format

--- test/pelemay_fp/parallel_splitter_test.exs
+++ test/pelemay_fp/parallel_splitter_test.exs
@@ -4,7 +4,17 @@ defmodule ParallelSplitterTest do
 
   test "single" do
     [{{pid, ref}, 0}] =
-      PelemayFp.ParallelSplitter.split({SplitterHelper, :count}, self(), 0, 1..10, 10, [], [:monitor])
+      PelemayFp.ParallelSplitter.split(
+        {SplitterHelper, :count},
+        self(),
+        0,
+        1..10,
+        10,
+        [],
+        [
+          :monitor
+        ]
+      )
 
     receive do
       {:DOWN, ^ref, :process, ^pid, :normal} ->
@@ -23,7 +33,17 @@ defmodule ParallelSplitterTest do
 
   test "double" do
     [{{_pid1, _ref1}, 1}, {{_pid0, _ref0}, 0}] =
-      PelemayFp.ParallelSplitter.split({SplitterHelper, :count}, self(), 0, 1..20, 10, [], [:monitor])
+      PelemayFp.ParallelSplitter.split(
+        {SplitterHelper, :count},
+        self(),
+        0,
+        1..20,
+        10,
+        [],
+        [
+          :monitor
+        ]
+      )
 
     assert PelemayFp.ParallelSplitter.range(1..20, 10) == 1..0
 
